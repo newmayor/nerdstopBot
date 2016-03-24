3c12d2e97b61756aecd0f652a90e7429f1d156c2
Numair Ahmed <numairahmed1@gmail.com>
2016-03-23 18:58:15 -0700

    Initial commit

---
+++ nerdstopBot.ino
@@ -0,0 +1,70 @@
+#include <AFMotor.h>
+
+
+#define echoPin 6
+#define trigPin 7
+
+char user_input;
+
+AF_DCMotor motor1(1, MOTOR12_64KHZ);
+AF_DCMotor motor2(2, MOTOR12_64KHZ);
+
+
+
+void setup() {
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+ // pinMode(led, OUTPUT);//
+ // pinMode(led2, OUTPUT);//
+
+}
+
+void loop() {
+  long duration, distance;
+  digitalWrite(trigPin, LOW);  // Added this line
+  delayMicroseconds(2); // Added this line
+  digitalWrite(trigPin, HIGH);
+//  delayMicroseconds(1000); - Removed this line
+  delayMicroseconds(10); // Added this line
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+  if (distance < 40) {  // This is where the LED On/Off happens
+     motor1.setSpeed(100);
+     motor2.setSpeed(100);
+      //"distance < 40" = object is too close
+      Serial.println("Object detected within 40 centimeters if robot\n");
+      motor1.run(RELEASE);  //stop motors
+      motor2.run(RELEASE);  //stop motors
+
+      Serial.println("Reversing robot to avoid object\n");
+      motor1.run(BACKWARD);  //move robot backward
+      motor2.run(FORWARD);  //move robot backward
+      Serial.println("Object is now more than 40 centimeters away\n");
+//     digitalWrite(led,HIGH); // When the Red condition is met, the Green LED should turn off
+  //   digitalWrite(led2,LOW);
+}
+  else {
+    Serial.println("Speed up and begin moving forward");
+      motor1.run(200);
+      motor2.run(200);
+
+      motor1.run(FORWARD);
+      motor2.run(BACKWARD);
+   // digitalWrite(led,LOW);
+   // digitalWrite(led2,HIGH);
+  }
+  if (distance >= 200 || distance <= 0){
+    motor1.setSpeed(200);
+      motor2.setSpeed(200);
+      Serial.print("Out of Range");
+      motor1.run(BACKWARD);
+      motor2.run(FORWARD);
+  }
+  else {
+    Serial.print(distance);
+    Serial.println(" cm");
+  }
+  delay(500);
+}
